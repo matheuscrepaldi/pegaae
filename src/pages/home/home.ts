f9069d8f4d25cdcd4d661acc1ad8f87a85acf652
Matheus Ribeiro <matheus_crepaldi_1991@hotmail.com>
2018-02-07 22:21:00 -0200

    splash screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private splashScreen: SplashScreen) {
+
+    this.splashScreen.show();
+    
+    this.splashScreen.hide();
 
   }
+
+  
 
 }
